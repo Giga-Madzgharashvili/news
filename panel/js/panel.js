8065e0d9af3b4cc3aaa26e26f43e3b57face7188
giga <gmadzgharashvili@gmail.com>
2022-10-22 15:42:10 +0400

    css autoprefixer

--- panel/js/panel.js
+++ panel/js/panel.js
@@ -1,14 +1,12 @@
 import { focusInput } from "../../js/module.js";
 
-
 fetch("https://newsfakedata.herokuapp.com/posts")
-.then((response) => response.json())
-.then((response) => {
-  response.forEach((element) => {
-   
-    createPost(element);
+  .then((response) => response.json())
+  .then((response) => {
+    response.forEach((element) => {
+      createPost(element);
+    });
   });
-});
 const panelPosts = document.getElementById("panelPosts");
 function createPost(event) {
   const div = document.createElement("div");
@@ -30,15 +28,13 @@ function createPost(event) {
 
   deletePost.addEventListener("click", (event) => {
     let id = event.target.getAttribute("data-id");
-    
+
     fetch(`https://newsfakedata.herokuapp.com/posts/${id}`, {
       method: "DELETE",
     });
     setTimeout(() => {
       window.location.reload();
     }, 1000);
-   
-    
   });
 }
 // add posts
@@ -47,43 +43,37 @@ const addBtn = document.getElementById("addBtn");
 const addover = document.getElementById("addover");
 const addForm = document.getElementById("addForm");
 
-
-addBtn.addEventListener("click",  () => {
+addBtn.addEventListener("click", () => {
   addover.classList.add("addactive");
 });
 
-
-addForm.addEventListener("submit", (event) =>{
+addForm.addEventListener("submit", (event) => {
   event.preventDefault();
- 
+
   const sendData = {
     title: event.target[0].value,
     // კონსოლში აგდებს შეცდომას ამის გარეშე და ამიტომ გავატანე იმიჯი. ფაილის ატვირთვა ვერ გავაკეთე :(
     image: event.target[2].value,
-    content: event.target[1].value
-  }
+    content: event.target[1].value,
+  };
 
-  fetch('https://newsfakedata.herokuapp.com/posts/', {
-  method: 'POST',
-  body: JSON.stringify(sendData),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then(() => {
-    setTimeout(() => {
-      addover.classList.remove("addactive");
-      window.location.reload();
-    }, 1000);
-  });
-
-})
-
+  fetch("https://newsfakedata.herokuapp.com/posts/", {
+    method: "POST",
+    body: JSON.stringify(sendData),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then(() => {
+      setTimeout(() => {
+        addover.classList.remove("addactive");
+        window.location.reload();
+      }, 1000);
+    });
+});
 
 const input = document.getElementById("addTitle");
-const addContent = document.getElementById("addContent")
-  input.addEventListener("focus", focusInput(input));
-  addContent.addEventListener("focus", focusInput(addContent));
-
-
+const addContent = document.getElementById("addContent");
+input.addEventListener("focus", focusInput(input));
+addContent.addEventListener("focus", focusInput(addContent));
